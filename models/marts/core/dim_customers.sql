b3b693cd4365ddabe5a7eeef40ff5726c985b157
jordanlapid@gmail.com <jordanlapid@gmail.com>
2022-06-12 16:14:28 +0000

    modularized code

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,14 +1,12 @@
+with customers as (
 
+    select * from {{ref("stg_customers")}}
+
+),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from dbt-tutorial.jaffle_shop.orders
+    select * from {{ref("stg_orders")}}
 
 ),
 
